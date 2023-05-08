182d7e3b7ce3df1df2074fe1cf6c8e98874b39e0
alexakten <aleakt@hotmail.com>
2023-05-08 09:17:43 +0200

    changed text

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'I am just testing';
+  const name = 'Hey there Kindred';
   greetUser(name);
   document.body.style.backgroundColor = 'blue';
 });
