6dd0024806831612d50a4e5f57e4c8a3c78696ff
Alex <alex.akten@outlook.com>
2024-07-24 14:11:06 +0200

    latest update

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Kontakt() {
   return (
     <main className="bg-blue-200 h-screen w-screen">
       <section className="w-screen h-screen flex flex-col items-center justify-center p-5 text-center">
-        <h1 className="text-red-400 text-8xl">Hej där</h1>
+        <h1 className="text-red-400 text-8xl">Kontaktsidan</h1>
         <p className="mt-8 ">Jag bygger hemsidor</p>
-        <Link href="/kontakt" className="mt-8 px-8 py-2 bg-blue-600 rounded-md text-white hover:bg-blue-700">
-          Klicka här{" "}
+        <Link href="/" className="mt-8 px-8 py-2 bg-blue-600 rounded-md text-white hover:bg-blue-700">
+          Gå hem{" "}
         </Link>
       </section>
     </main>
