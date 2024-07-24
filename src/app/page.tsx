49a80b5d62f340edf03e84d723631e0f7d29d8a3
Alex <alex.akten@outlook.com>
2024-07-24 14:14:49 +0200

    bytte text igen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="bg-blue-200 h-screen w-screen">
       <section className="w-screen h-screen flex flex-col items-center justify-center p-5 text-center">
-        <h1 className="text-red-400 text-8xl">Hej där</h1>
+        <h1 className="text-red-400 text-8xl">Hej på dig</h1>
         <p className="mt-8 ">Jag bygger bilar</p>
         <Link href="/kontakt" className="mt-8 px-8 py-2 bg-blue-600 rounded-md text-white hover:bg-blue-700">
           Klicka här{" "}
